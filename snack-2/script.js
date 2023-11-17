c94e4e5f81e8c066947e9c9fe5da6686fc01b0b3
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-17 12:01:13 +0100

    creata lista e ciclo for

---
+++ snack-2/script.js
@@ -0,0 +1,8 @@
+const listaNomi = [mario, maria, gino, ginetta, paolo, sandrone, carmelo, carmela, titti, pluto]
+const listaCognomi = [pietra, sasso, carta, forbice, falegname, fungo, penna, telefono, cavo]
+
+for (i=0; i<10; i++){
+    let numRandom1 = Math.floor(Math.random() * 10) * 1
+    let numRandom2 = Math.floor(Math.random() * 10) * 1
+
+}
