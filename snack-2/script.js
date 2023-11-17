18bfca7ab7b12db890449f01a25fc7776e0a3f9a
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-17 12:13:58 +0100

    ins numero di invi. e usato come cond. interativa

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,11 +1,12 @@
 const listaNomi = ["mario", "maria", "gino", "ginetta", "paolo", "mario", "maria", "gino", "ginetta", "paolo"]
 const listaCognomi = ["pietra", "sasso", "carta", "forbice", "falegname", "pietra", "sasso", "carta", "forbice",]
+const numeroInvitati = 20
 
 console.log(listaNomi.length, listaCognomi.length)
 
 let PersonaInvitata;
 
-for (i=0; i<10; i++){
+for (i=0; i<numeroInvitati; i++){
     let numRandom1 = Math.floor(Math.random() * listaNomi.length) * 1
     let numRandom2 = Math.floor(Math.random() * listaCognomi.length) * 1
 
