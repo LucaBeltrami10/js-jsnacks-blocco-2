23d446d86a1c5d123eee8171a09204facb0d90e3
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-17 12:05:02 +0100

    inserito operazioni dentro al ciclo

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,8 +1,16 @@
 const listaNomi = [mario, maria, gino, ginetta, paolo, sandrone, carmelo, carmela, titti, pluto]
 const listaCognomi = [pietra, sasso, carta, forbice, falegname, fungo, penna, telefono, cavo]
 
+let PersonaInvitata;
+
 for (i=0; i<10; i++){
     let numRandom1 = Math.floor(Math.random() * 10) * 1
     let numRandom2 = Math.floor(Math.random() * 10) * 1
 
+    let nomeInvitato = listaNomi[1]
+    let cognomeInvitato = listaCognomi[1]
+
+    PersonaInvitata = `${nomeInvitato} ${cognomeInvitato}`
+
+    console.log(`persona invitata: ${PersonaInvitata}`)
 }
