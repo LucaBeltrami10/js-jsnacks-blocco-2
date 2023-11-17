678cd6709270147931d42c1d42d88c1fe6b45f34
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-17 11:40:46 +0100

    inseriti console.log di controllo

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,12 +1,16 @@
 const numbersEntered = []
 let userNumber = parseInt(prompt('inserisci un numero'))
+console.log(`primo numero inserito ${userNumber}`)
 
 let numbersEnteredSum = 0
 
 while (numbersEnteredSum < 50){
+    userNumber = parseInt(prompt('inserisci un nuovo numero'))
+    console.log(`numero inserito ${userNumber}`)
     numbersEntered.push(userNumber)
     numbersEnteredSum += userNumber
-    userNumber = parseInt(prompt('inserisci un nuovo numero'))
+    
 }
 
 console.log(`i numeri inseriti nella lista sono: ${numbersEntered}`)
+console.log(`la loro somma è: ${numbersEnteredSum}`)
