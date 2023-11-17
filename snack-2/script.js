dfcb2866881e899ad7910de6d444dd02432a5efd
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-17 12:07:01 +0100

    coretto array

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,5 +1,5 @@
-const listaNomi = [mario, maria, gino, ginetta, paolo, sandrone, carmelo, carmela, titti, pluto]
-const listaCognomi = [pietra, sasso, carta, forbice, falegname, fungo, penna, telefono, cavo]
+const listaNomi = ["mario", "maria", "gino", "ginetta", "paolo"]
+const listaCognomi = ["pietra", "sasso", "carta", "forbice", "falegname"]
 
 let PersonaInvitata;
 
@@ -7,10 +7,10 @@ for (i=0; i<10; i++){
     let numRandom1 = Math.floor(Math.random() * 10) * 1
     let numRandom2 = Math.floor(Math.random() * 10) * 1
 
-    let nomeInvitato = listaNomi[1]
-    let cognomeInvitato = listaCognomi[1]
+    let nomeInvitato = listaNomi[numRandom1]
+    let cognomeInvitato = listaCognomi[numRandom2]
 
-    PersonaInvitata = `${nomeInvitato} ${cognomeInvitato}`
+    PersonaInvitata = nomeInvitato + cognomeInvitato
 
     console.log(`persona invitata: ${PersonaInvitata}`)
 }
