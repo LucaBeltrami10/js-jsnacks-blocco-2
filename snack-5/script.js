6be346e9a86835640de0c95b306cee5049b07cf3
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-20 12:31:10 +0100

    creata funzione. generato parent

---
+++ snack-5/script.js
@@ -0,0 +1,7 @@
+/* creo funzione per generare padre e figlio */
+
+function addParentAndChildText (tagParent, TagChild, TextChild){
+    /* genero padre */
+    document.createElement('ul')
+    
+}
