84d2505023a24417df1713b9f5b5704be82c79a6
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-20 11:45:04 +0100

    inserita condizione in funzione e return.

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,6 +1,15 @@
-const firstWord = prompt(inserisci una parola)
-const secondtWord = prompt(inserisci una parola)
+const firstWord = prompt('inserisci una parola')
+const secondtWord = prompt('inserisci una parola')
+
+console.log(firstWord.length)
 
 function whichOneIsLonger (firstWord, secondtWord){
-    
+
+    if (firstWord.length > secondtWord.length){
+        return console.log(firstWord)
+    } else if (secondtWord.length > firstWord.length){
+        return console.log(secondtWord)
+    }else{
+        return console.log('le parole sono lunghe uguali')
+    }
 }
