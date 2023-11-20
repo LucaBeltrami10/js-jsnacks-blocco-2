7dc38c528de965f71ea7cbdc3da4eaf3b9df2b79
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-20 12:05:07 +0100

    inseriti commenti

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,9 +1,12 @@
+/* richiedo due parole */
 const firstWord = prompt('inserisci una parola')
 const secondWord = prompt('inserisci una parola')
 
+/* controllo la lunghezza delle parole */
 console.log(firstWord.length)
 console.log(secondWord.length)
 
+/* creo funzione con parole chiave */
 function whichOneIsLonger (wordA, wordB){
 
     if (wordA.length > wordB.length){
@@ -15,5 +18,8 @@ function whichOneIsLonger (wordA, wordB){
     }
 }
 
+/* uso funzione e inserisco le parole prompt da lavorare */
 let LongerWord = whichOneIsLonger(firstWord, secondWord)
+
+/* mostro il return della funzione */
 console.log(LongerWord)
