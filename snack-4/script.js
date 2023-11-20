484777d41ff77ee673f309a2e5c3d9b762590350
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-20 12:16:38 +0100

    usata funzione per numeroi richiesti user

--- snack-4/script.js
+++ snack-4/script.js
@@ -1,9 +1,17 @@
-function (firstNumber, secondNumber){
-    if (parseInt(firstNumber) % parseInt(secondNumber) == 0){
+function firstIsDivisibleBySecond (numberA, numberB){
+    if (parseInt(numberA) % parseInt(numberB) == 0){
         return true
     }else{
         return false
     }
 }
 
+/* richiedo due numeri all'user */
+const firstNumber = prompt('inserisci un numero')
+const secondNumber = prompt('inserisci un secondo numero')
 
+/* uso la funzione per determinare se il primo è divisibile per il secondo */
+let firstIsDivisible = firstIsDivisibleBySecond(firstNumber, secondNumber)
+
+/* mostro in console il return della funzione */
+console.log(firstIsDivisible)
