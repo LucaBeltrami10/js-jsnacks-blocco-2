7c1991632544fd6e093077bbcd44e3ad3e0cf424
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-20 12:36:32 +0100

    creato corpo funzione

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,7 +1,16 @@
 /* creo funzione per generare padre e figlio */
 
 function addParentAndChildText (tagParent, TagChild, TextChild){
-    /* genero padre */
-    document.createElement('ul')
-    
+    /* genero parent */
+    let newParent = document.createElement(tagParent)
+    /* genero figlio e lo inserisco in parent */
+    let newChild = document.createElement(TagChild)
+    newParent.appendchild(newChild)
+    /* inserisco testo in child */
+    newChild.innerHTML = TextChild
 }
+
+/* recupero il tag a cui aggiungere parent e child */
+const divParent = document.getElementById('container')
+
+
