0fe4123cd1ce7a2d9ae193d8899471528d91a693
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-20 12:44:12 +0100

    corretto snack

--- snack-5/script.js
+++ snack-5/script.js
@@ -15,4 +15,6 @@ function addParentAndChildText (tagParent, TagChild, TextChild){
 /* recupero il tag a cui aggiungere parent e child */
 const divParent = document.getElementById('container')
 
+/* uso funzione per generare elementi all'interno e collego padre */
+
 
