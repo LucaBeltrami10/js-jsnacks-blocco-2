498ca3ded9b2737ca5e31e6ce938b12f853c2fc3
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-21 12:18:46 +0100

    inserito ciclo in funzione. inserito arg  parent

--- snack-6/script.js
+++ snack-6/script.js
@@ -1,7 +1,12 @@
 /* genero funzione */
-function(numElements){
-    let element = document.createElement(div)
-    element.classlist.add('square-element')
+function addNumberOfChildToParent(numElements, parentId){
+
+    for (let i = 0 ; i < numElements ; i++){
+        let element = document.createElement(div)
+        element.classlist.add('square-element')
+
+        let parent = document.querySelector('div')
+    }
 }
 
 
