1335aa332b985cef6790dd4d524d23f6fbc73ede
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-23 12:22:57 +0100

    creata funzione per aggiungere tempo

--- crono-snack/script.js
+++ crono-snack/script.js
@@ -2,8 +2,13 @@ const start = document.getElementById('start')
 const stop = document.getElementById('stop')
 const tempo = document.getElementById('tempo')
 
-let tempoTrascorso =
+let tempoTrascorso = 0
 
 start.addEventListener('click', function(){
-
+    setTimeout()
 })
+
+
+function addSecond(conteggioTempo){
+    conteggioTempo += 0.1
+}
