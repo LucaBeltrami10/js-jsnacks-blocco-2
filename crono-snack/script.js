afa49a655af84565c208882272bd649c957a251e
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-23 12:19:49 +0100

    aggiunto event listener su start

--- crono-snack/script.js
+++ crono-snack/script.js
@@ -1,3 +1,7 @@
 const start = document.getElementById('start')
 const stop = document.getElementById('stop')
 const tempo = document.getElementById('tempo')
+
+start.addEventListener('click', function(){
+    
+})
