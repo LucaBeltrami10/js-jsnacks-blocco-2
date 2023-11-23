5867b198b4d531a999899afb45779166480ea0da
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-23 12:20:58 +0100

    creata variabile per tempo trascorso

--- crono-snack/script.js
+++ crono-snack/script.js
@@ -2,6 +2,8 @@ const start = document.getElementById('start')
 const stop = document.getElementById('stop')
 const tempo = document.getElementById('tempo')
 
+let tempoTrascorso =
+
 start.addEventListener('click', function(){
-    
+
 })
