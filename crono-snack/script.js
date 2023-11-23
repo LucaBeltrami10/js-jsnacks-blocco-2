021d2ab8f6aab7a08e575e2e60d2a77551d92fb5
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-23 13:58:06 +0100

    commentato esercizio sbagliato

--- crono-snack/script.js
+++ crono-snack/script.js
@@ -2,14 +2,29 @@ const start = document.getElementById('start')
 const stop = document.getElementById('stop')
 const tempo = document.getElementById('tempo')
 
-let tempoTrascorso = 0
+
+
+
+
+
+
+
+
+/* let tempoTrascorso; 
 
 start.addEventListener('click', function(){
-    setInterval(addSecondInDecimali, 100, tempoTrascorso);
+    let clock = setInterval(function(
+        tempoTrascorso += 1
+        console.log(tempoTrascorso)
+    ) 1000);
+})
+
+stop.addEventListener('click',function(){
+    clock = clearInterval(clock)
 })
 
 
 function addSecondInDecimali(conteggioTempo){
-    conteggioTempo += 0.1
+    conteggioTempo += 1;
     console.log(conteggioTempo)
-}
+    /* conteggioTempo.innerHTML = parseInt(conteggioTempo, 10) + 1 */
