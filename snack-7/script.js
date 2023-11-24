26abbbb3f791757679404f486482357a59408751
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-24 12:27:45 +0100

    aggiunta virgola dopo ogni oggetto

--- snack-7/script.js
+++ snack-7/script.js
@@ -2,39 +2,39 @@ const zucchine = [
     {'varietà' : 'nera',
     'peso' : '',
     'lunghezza' : ''
-    }
+    },
+    {'varietà' : 'romanesca',
+    'peso' : '',
+    'lunghezza' : ''
+    },
+    {'varietà' : 'fiorentina',
+    'peso' : '',
+    'lunghezza' : ''
+    },
+    {'varietà' : 'napoletana',
+    'peso' : '',
+    'lunghezza' : ''
+    },
+    {'varietà' : 'tonda',
+    'peso' : '',
+    'lunghezza' : ''
+    },
+    {'varietà' : 'trombetta',
+    'peso' : '',
+    'lunghezza' : ''
+    },
     {'varietà' : '',
     'peso' : '',
     'lunghezza' : ''
-    }
+    },
     {'varietà' : '',
     'peso' : '',
     'lunghezza' : ''
-    }
+    },
     {'varietà' : '',
     'peso' : '',
     'lunghezza' : ''
-    }
-    {'varietà' : '',
-    'peso' : '',
-    'lunghezza' : ''
-    }
-    {'varietà' : '',
-    'peso' : '',
-    'lunghezza' : ''
-    }
-    {'varietà' : '',
-    'peso' : '',
-    'lunghezza' : ''
-    }
-    {'varietà' : '',
-    'peso' : '',
-    'lunghezza' : ''
-    }
-    {'varietà' : '',
-    'peso' : '',
-    'lunghezza' : ''
-    }
+    },
     {'varietà' : '',
     'peso' : '',
     'lunghezza' : ''
