f1f00c38dc7d18a1ebcee454ab906800cdb98ede
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-24 12:30:50 +0100

    inseriti dati in oggetti

--- snack-7/script.js
+++ snack-7/script.js
@@ -1,42 +1,42 @@
 const zucchine = [
     {'varietà' : 'nera',
-    'peso' : '',
-    'lunghezza' : ''
+    'peso' : '1',
+    'lunghezza' : '50'
     },
     {'varietà' : 'romanesca',
-    'peso' : '',
-    'lunghezza' : ''
+    'peso' : '0.3',
+    'lunghezza' : '12'
     },
     {'varietà' : 'fiorentina',
-    'peso' : '',
-    'lunghezza' : ''
+    'peso' : '0.6',
+    'lunghezza' : '10'
     },
     {'varietà' : 'napoletana',
-    'peso' : '',
-    'lunghezza' : ''
+    'peso' : '0.2',
+    'lunghezza' : '17'
     },
     {'varietà' : 'tonda',
-    'peso' : '',
-    'lunghezza' : ''
+    'peso' : '0.6',
+    'lunghezza' : '5'
     },
     {'varietà' : 'trombetta',
-    'peso' : '',
-    'lunghezza' : ''
+    'peso' : '0.8',
+    'lunghezza' : '18'
     },
-    {'varietà' : '',
-    'peso' : '',
-    'lunghezza' : ''
+    {'varietà' : 'gialla',
+    'peso' : '0.9',
+    'lunghezza' : '15'
     },
-    {'varietà' : '',
-    'peso' : '',
-    'lunghezza' : ''
+    {'varietà' : 'rugosa friulana',
+    'peso' : '1.1',
+    'lunghezza' : '14'
     },
-    {'varietà' : '',
-    'peso' : '',
-    'lunghezza' : ''
+    {'varietà' : 'patisson',
+    'peso' : '2',
+    'lunghezza' : '30'
     },
-    {'varietà' : '',
-    'peso' : '',
-    'lunghezza' : ''
+    {'varietà' : 'crookneck',
+    'peso' : '0.5',
+    'lunghezza' : '10'
     }
 ]
